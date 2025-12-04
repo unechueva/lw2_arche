ba34e333c2db1a5621c8090a27beb2893eea03ab
unechueva <uliana.nechueva@gmail.com>
2025-12-04 18:25:02 +0300

    feat: готовый и итоговый вариант сервера и структуры

--- include/session.h
+++ include/session.h
@@ -1,17 +1,18 @@
 #ifndef SESSION_H
 #define SESSION_H
 
-#include <winsock2.h>
+#include <sys/socket.h> // Для типа socket в Linux
 #include <chrono>
 #include <string>
 
 struct Session {
-    SOCKET socket;
+    int socket; // в Linux сокет — это int
     std::chrono::steady_clock::time_point last_active;
     std::string client_name; // добавлено
     std::string state; // уже есть
 };
 
-void update_last_active(Session& session);
+// Объявление функции
+//void update_last_active(Session& session);
 
-#endif
+#endif // SESSION_H
--- src/server/session.cpp
+++
@@ -1,5 +0,0 @@
-#include "../../include/session.h"
-
-void update_last_active(Session& session) {
-    session.last_active = std::chrono::steady_clock::now();
-}
